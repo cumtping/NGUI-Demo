9d4e1430782a14e8f225484dc60af97208c4509a
WenpingWang <wenping0820@163.com>
2016-05-20 00:34:20 +0800

    First commit
    1 菜单页实现；
    2 帮助页实现；
    3 --添加音效；
    4 --添加场景跳转；

---
+++ Assets/Scripts/HelpPanelCloseButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpPanelCloseButton : MonoBehaviour {
+	public GameObject helpPanelObj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClick(){
+		iTween.MoveTo(helpPanelObj, new Vector3(-Screen.width / 2, Screen.height / 2, 0), 1);
+	}
+}
