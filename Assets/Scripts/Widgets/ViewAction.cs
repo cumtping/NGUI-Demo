99e4edcf5c7874a6bbccd99f5c0f87c30eda014e
WenpingWang <wenping0820@163.com>
2016-05-22 23:45:32 +0800

    Toggle控件、Slider控件使用
    1 使用Toggle控制背景音乐开关；
    https://www.youtube.com/watch?v=0ewSSlTG2xo
    2 使用Slider控制背景音量；
    https://www.youtube.com/watch?v=HQ8Tttcksu4
    3 Toggle Group 单选框；
    4  AudioSource使用
        https://www.youtube.com/watch?v=_ULjnRMxY0c

---
+++ Assets/Scripts/Widgets/ViewAction.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewAction : MonoBehaviour {
+	public AudioSource bgMusic;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void musicSwitch(bool newValue) {
+		if (newValue) {
+			bgMusic.Play ();
+		} else {
+			bgMusic.Stop ();
+		}
+	}
+
+	public void musicVolume(float newValue) {
+		bgMusic.volume = newValue;
+	}
+}
